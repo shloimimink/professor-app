43a47244222a80980eb24b6a535efd46a2af9358
shloimimink <shloimimink@gmail.com>
2020-02-20 19:17:40 -0600

    full crud professors added

--- queries.js
+++ queries.js
@@ -7,7 +7,7 @@ const pool = new Pool({
     port: '5432'
 });
 
-
+// INDEX PROFESSORS
 const getAllProfessors = (req, res) => {
     pool.query('SELECT * FROM professors ORDER BY id ASC', (err, results) => {
         if (err) {
@@ -17,6 +17,19 @@ const getAllProfessors = (req, res) => {
     })
 };
 
+// SHOW PROFESSOR
+const getProfessorById = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pool.query('SELECT * FROM professors WHERE id = $1', [id], (err, results) => {
+        if (err) {
+            throw err
+        }
+        res.status(200).json(results.rows)
+    })
+};
+
+// CREATE PROFESSOR
 const addProfessor = (req, res) => {
     const {name, title, school, department, difficulty} = req.body;
 
@@ -30,5 +43,36 @@ const addProfessor = (req, res) => {
     })
 };
 
+// UPDATE PROFESSOR
+const updateProfessorById = (req, res) => {
+    const id = parseInt(req.params.id);
+    const {name, title, school, department, difficulty} = req.body;
 
-module.exports = {getAllProfessors, addProfessor};
+    pool.query('UPDATE professors SET name = $1, title = $2, school = $3, department = $4, difficulty = $5 WHERE id = $6' +
+        ' RETURNING id', [name, title, school, department, difficulty, id], (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.status(200).json(result.rows[0])
+    })
+};
+
+// DELETE PROFESSOR
+const deleteProfessorById = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pool.query('DELETE FROM professors WHERE id = $1', [id], (err) => {
+        if (err) {
+            throw err
+        }
+        res.status(200).json(`User deleted with ID: ${id}`)
+    })
+};
+
+module.exports = {
+    getAllProfessors,
+    addProfessor,
+    getProfessorById,
+    updateProfessorById,
+    deleteProfessorById
+};
